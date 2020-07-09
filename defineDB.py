ee02edff18c73b186a6ac483bfe40870c4143160
jlowy95 <jlowy95@gmail.com>
2020-07-09 11:10:40 -0700

    Restructuring from objects to JSON

--- defineDB.py
+++ defineDB.py
@@ -1,22 +1,11 @@
 # Author Josh Lowy
 
-# Define/Initialize basic parameters of the ticklist database such as the continent. 
-# 'Area' object will be parent object for continent, 'Sub-Area' will be child class 
-# for any/all actual areas with climbs.  Continents will be individual collections in the DB.
+# Connect to ticklist database
+# Define functions for adding areas and climbs
 
-import pymongo
+from pymongo import MongoClient
 
+# Setup db client and database
+client = MongoClient('mongodb://localhost:27017/')
+db = client.ticklist
 
-# initalizeContinents
-# Creates an 'area' object for each of the continents except Antarctica
-
-# addNewArea
-# Will create a new subArea object in the currently selected area
-# Properties (* denotes required at initialization):
-# Name*, Parent (*Auto-filled by current selection), Description
-
-# addNewClimb
-# Will create a new climb object in the currently selected area
-# Properties (* denotes required at initialization):
-# Name*, Parent (*Auto-filled by current selection), Grade*, Danger (Default to None),
-# Quality*, Description, Height, FA
