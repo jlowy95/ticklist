87f99213824c002b9f9ed96cce42b87bd588560a
jlowy95 <jlowy95@gmail.com>
2020-07-10 11:41:24 -0700

    Began writing connection between front and back end

--- app.py
+++ app.py
@@ -4,7 +4,25 @@
 # Connects to local MongoDB and allows for easy maintenance and logging of
 # ticked climbs and tracking of ascents.
 
-import Flask
-import flask_pymongo
+from flask import Flask, render_template, redirect, request, jsonify
+from flask_pymongo import PyMongo
+import json
+import pprint
 
-client = 
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/ticklist"
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/navDB', methods=['POST'])
+def predict_rent_price():
+    collection = request.get_json()
+
+if __name__ == '__main__':
+    app.run(debug=True)
