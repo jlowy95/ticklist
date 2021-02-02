df3c0a25804aa475e48633b7a59b878bbd8567f4
jlowy95 <jlowy95@gmail.com>
2021-02-01 22:23:19 -0800

    More progress on mass import + tweaks to html layout

--- static/javascript/massimport.js
+++ static/javascript/massimport.js
@@ -1,6 +1,25 @@
 // massimport.js: Handle file uploads for the mass importer tool and parse csv files for client-side 
 // data validation, then push to server for submission.
 
+// -------------- PSUEDO -------------------
+// 1. Input csv file
+// 2. Read csv
+// 3. Iterate by row
+// 4. Validate data
+//   a. Check for duplicate
+//   b. Validate data
+//     1. Check climb_type
+//     2. Check grade
+//     3. Check quality & danger (required)
+//     4. Optionally validate other details
+//     - If valid, addEntry
+//     - If invalid, pause, attempt to fix, REvalidate
+// 5. Locate entry insert or create new area
+// 6. Update user (status)
+
+
+// -------------- CODE ---------------------
+
 // Define "submit" button
 var continueButton = $('#continue');
 continueButton.on('click', processFile);
@@ -71,35 +90,74 @@ function nextRow() { //Iterate row by row of imported csv file
         // Separate details
         currentRow = mifile[currentIndex].split(',');
         var entry = separateDetails(currentRow);
-        console.log(entry);
+        console.log(entry.name);
 
         // Check Database
         // var check = checkEntry(entry);
         // console.log(`check:${check}`);
         // getEntry(entry);
-        
 
         // Clear contents of previous entry
         $('#areas').empty();
         $('#climb').empty();
         $('#details').empty();
+
         // Populate contents of current entry
-        for (var i = 0; i<entry.areas.length; i++) {
+        for (let i = 0; i<entry.areas.length; i++) {
             $('#areas').append($('<span></span>').text(entry.areas[i]));
             if (i<entry.areas.length-1 && entry.areas[i+1]!='') {
                 $('#areas').append(' > ');
             }
         }
-        $('#climb').append($('<span></span>').text(entry.name));
-        for (var i = 0; i<entry.details.length; i++) {
-            $('#details').append($('<span></span>').text(entry.details[i]));
-            if (i<entry.details.length-1) {
-                $('#details').append(', ');
+        $('#climb').text(entry.name);
+        $('#grade').text(function() {
+            if (entry.details.climb_type == 'boulder') {
+                return 'V' + entry.details.grade;
+            } else if (entry.details.climb_type == 'route') {
+                return '5.' + entry.details.grade;
+            } else {
+                return 'N/A';
             }
-        }
+        });
+        $('#danger').text(function() {
+            switch (entry.details.danger) {
+                case '0':
+                    return '';
+                case '1':
+                    return 'PG-13';
+                case '2':
+                    return 'R';
+                case '3':
+                    return 'X';
+                default:
+                    return 'N/A';
+            }
+        });
+        $('#quality').html(function() {
+            var quality = '';
+            for (let i=0; i<entry.details.quality;i++) {
+                quality += '&#9733';
+            }
+            return quality;
+        });
+        
+        // for (let detail in entry.details.keys) {
+
+        // }
+        // for (let i in entry.details.keys) {
+        //     $('#details').append($('<span></span>').text(entry.details[i]));
+        //     if (i<entry.details.keys.length-1) {
+        //         $('#details').append(', ');
+        //     }
+        // }
+
         // add current row to table
         addTRow(entry);
+        // Check for duplicate, if dupe, continue
         dupeCheck(entry);
+
+        gradeCheck(entry);
+
     } else {
         alert('Temp Done!');
     }
@@ -132,13 +190,13 @@ function checkEntry(entry) {
 
 
 async function getEntry(entry) {
-    for (var i=0; i<entry.areas.length; i++) {
+    for (let i=0; i<entry.areas.length; i++) {
         await fetch(`/check-entry?area=${entry.areas[i]}`, {
             method: 'GET'
         })
         .then (response => response.json())
         .then (function(data) {
-            console.log(data);
+            // console.log(data);
         })
         .catch(error => console.log(error));
     }
@@ -151,10 +209,13 @@ function dupeCheck(entry) {
     })
     .then (response => response.json())
     .then (function(data) {
+        // If duplicate was found, update status
         if (data.dupeCheck) {
             $(`#tr-${currentIndex+1} td:last span`).css('background', 'blue');
+            // Continue
+            iterateRow();
         }
-        console.log(data);
+        // console.log(data);
     })
     .catch(error => console.log(error));
 }
@@ -183,6 +244,10 @@ function separateDetails(row) {
     // Object Assignment
     entry.areas = parents;
     entry.name = row[6];
+    // If name was omitted, skip
+    if (entry.name == '') {
+        iterateRow();
+    }
     entry.details.climb_type = row[7];
     entry.details.grade = row[8];
     entry.details.pitches = row[9];
@@ -195,4 +260,44 @@ function separateDetails(row) {
     entry.details.description = row[16];
     entry.details.pro = row[17];
     return entry;
+}
+
+// gradeCheck:
+function gradeCheck(entry) {
+    // All inputs of type string as parsed by FileReader
+    // TEMPORARY: grades will be submitted as whole US grades w/o V or 5.
+    var climb_type = entry.details.climb_type.toLowerCase();
+    var grade = entry.details.grade.toLowerCase();
+    console.log('gradeCheck: ' + grade);
+
+    // Check for valid symbols
+    var valids = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','+','-','/'];
+    for (let i=0;i<grade.length;i++) {
+        // If character isn't valid...
+        if (!(valids.includes(grade[i]))) {
+            console.log('INVALID GRADE - Character: ' + grade[i]);
+            // User Correction Needed
+        }
+    }
+
+    // Boulder grades may have +/- at end
+    // if (climb_type == 'boulder') {
+    //     // Check for +/- in grade
+        
+    //     // Remove +/- if attached
+    //     if (grade.endswith('+') || grade.endswith('-')) {
+    //         grade = grade.slice(0,-1);
+    //     // Check that base integer is btw 0-16
+    //     if (parseInt(grade) >= 0 && parseInt(grade) <= 16) {
+    //         console.log('Valid grade');
+    //     } else {
+    //         console.log('INVALID GRADE - boulderNot0-16');
+    //     }
+
+    // } else if (climb_type == 'route') {
+        
+    // } else {
+    //     // Invalid climb_type
+        
+    // }
 }
